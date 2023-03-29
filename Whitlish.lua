41550dcfb4bdd61403bf09458f2c99795ab7d16d
Zeros <109470610+ZCAVGHub@users.noreply.github.com>
2023-03-29 15:15:06 +0700

    Update Whitlish.lua

--- Whitlish.lua
+++ Whitlish.lua
@@ -1,5 +1,10 @@
 local whitlist = {
   ["JQEZ-1C625BA7ECC4-9318-F71"] = {hwid="53897B20-2FBD-45CC-9F71-1C625BA7ECC4"},
+  ["none"] = {hwid="none"},
+  [""] = {hwid=""},
+  [""] = {hwid=""},
+  [""] = {hwid=""},
+  [""] = {hwid=""},
 }
 
 return whitlist
